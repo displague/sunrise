cc95c24e1d8d16bf4b37366120dc03874e6aa2a3
Marques Johansson <marques@displague.com>
2014-07-08 00:13:48 -0400

    added sprites and background

--- main.py
+++ main.py
@@ -3,36 +3,46 @@ import pygame
 class Game(object):
     def main(self, screen):
         clock = pygame.time.Clock()
-        
-        image = pygame.image.load('player.png')
-        x = 100 
-        y = 100
-        
+
+        background = pygame.image.load('background.png')
+        sprites = pygame.sprite.Group()
+        self.player = Player(sprites)
+
         while 1:
-            clock.tick(30)
+            dt = clock.tick(30)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
                 if event.type == pygame.KEYDOWN and \
-                        event.key == pygame.K_ESCAPE:
-                    return
-            
-            key = pygame.key.get_pressed()
-            if key[pygame.K_LEFT]:
-                    x -= 10
-            if key[pygame.K_RIGHT]:
-                    x += 10
-            if key[pygame.K_UP]:
-                    y -= 10
-            if key[pygame.K_DOWN]:
-                    y += 10
+                    event.key == pygame.K_ESCAPE:
+                        return
 
+
+            sprites.update(dt / 1000.)
             screen.fill((200,200,200))
-            screen.blit(image, (x,y))
+            screen.blit(background, (0,0))
+            sprites.draw(screen)
             pygame.display.flip()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super(Player, self).__init__(*groups)
+        self.image = pygame.image.load('player.png')
+        self.rect = pygame.rect.Rect((320,240), self.image.get_size())
+
+    def update(self, dt):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            self.rect.x -= 300 * dt
+        if key[pygame.K_RIGHT]:
+            self.rect.x += 300 * dt
+        if key[pygame.K_UP]:
+            self.rect.y -= 300 * dt
+        if key[pygame.K_DOWN]:
+            self.rect.y += 300 * dt
+
 if __name__ == '__main__':
-        pygame.init()
-        screen = pygame.display.set_mode((640,480))
-        Game().main(screen)
+    pygame.init()
+    screen = pygame.display.set_mode((640,480))
+    Game().main(screen)
