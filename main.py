fcb1f6c692094055834ae51f9ece78ea04040107
Marques Johansson <marques@displague.com>
2014-07-10 22:31:55 -0400

    adding tmx support.  a little broken atm. need to use tiled to add platform/blocker layers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-#import tmx
+import tmx
 
 class ScrolledGroup(pygame.sprite.Group):
     def draw(self, surface):
@@ -13,16 +13,14 @@ class Game(object):
         background = pygame.image.load('background.png')
         sprites = ScrolledGroup()
         sprites.camera_x = 0
-        self.player = Player(sprites)
         self.walls = pygame.sprite.Group()
-        block = pygame.image.load('block.png')
-        for x in range(0, 800, 32):
-            for y in range(0, 480, 32):
-                if x in (0, 800-32) or y in (0, 480-32):
-                    wall = pygame.sprite.Sprite(self.walls)
-                    wall.image = block
-                    wall.rect = pygame.rect.Rect((x, y), (10,15))
-        sprites.add(self.walls)
+
+        self.tilemap = tmx.load('desert.tmx', screen.get_size())
+
+        self.sprites = tmx.SpriteLayer()
+        start_cell = self.tilemap.layers['triggers'].find('player')[0]
+        self.player = Player((start_cell.px, start_cell.py), self.sprites)
+        self.tilemap.layers.append(self.sprites)
 
         while 1:
             dt = clock.tick(30)
@@ -35,17 +33,16 @@ class Game(object):
                             return
 
 
-            sprites.update(dt / 1000., self)
-            screen.fill((200,200,200))
+            self.tilemap.update(dt/1000.,self)
             screen.blit(background, (0,0))
-            sprites.draw(screen)
+            self.tilemap.draw(screen)
             pygame.display.flip()
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, *groups):
+    def __init__(self, location, *groups):
         super(Player, self).__init__(*groups)
         self.image = pygame.image.load('player.png')
-        self.rect = pygame.rect.Rect((320,240), self.image.get_size())
+        self.rect = pygame.rect.Rect(location, self.image.get_size())
         self.resting = False
         self.dy = 0
 
@@ -66,19 +63,21 @@ class Player(pygame.sprite.Sprite):
 
         new = self.rect
         self.resting = False
-        for cell in pygame.sprite.spritecollide(self, game.walls, False):
-            cell = cell.rect
-            if last.right <= cell.left and new.right > cell.left:
-                    new.right = cell.left
-            if last.left >= cell.right and new.left < cell.right:
+        for cell in game.tilemap.layers['triggers'].collide(new, 'blockers'):
+            blockers = cell['blockers']
+            if 'l' in blockers and last.right <= cell.left and new.right > cell.left:
+                new.right = cell.left
+            if 'r' in blockers and last.left >= cell.right and new.left < cell.right:
                     new.left = cell.right
-            if last.bottom <= cell.top and new.bottom > cell.top:
+            if 't' in blockers and last.bottom <= cell.top and new.bottom > cell.top:
                     self.resting = True
                     new.bottom = cell.top
                     self.dy = 0
-            if last.top >= cell.bottom and new.top < cell.bottom:
+            if 'b' in blockers and last.top >= cell.bottom and new.top < cell.bottom:
                     new.top = cell.bottom
                     self.dy = 0
+        game.tilemap.set_focus(new.x, new.y)
+
         self.groups()[0].camera_x = self.rect.x - 320
 
 
