e7daf0154d289fa84af2b15f97e877916f66bd45
Marques Johansson <marques@linode.com>
2016-04-23 23:19:16 -0400

    use inequality chains, fix pylint warnings (complexity)

--- libs/player.py
+++ libs/player.py
@@ -1,6 +1,8 @@
 import pygame
 
+
 class Player(pygame.sprite.Sprite):
+
     def __init__(self, location, *groups):
         super(Player, self).__init__(*groups)
         self.image = pygame.image.load('assets/images/player.png')
@@ -8,10 +10,7 @@ class Player(pygame.sprite.Sprite):
         self.resting = False
         self.dy = 300
 
-    def update(self, dt, game):
-        last = self.rect.copy()
-
-        key = pygame.key.get_pressed()
+    def _handle_key(self, key, dt):
         if key[pygame.K_LEFT]:
             self.rect.x -= 300 * dt
         if key[pygame.K_RIGHT]:
@@ -21,27 +20,35 @@ class Player(pygame.sprite.Sprite):
         if key[pygame.K_DOWN]:
             self.rect.y += 300 * dt
 
-        #if self.resting and key[pygame.K_SPACE]:
+    def _handle_blockers(self, last, new, game):
+        self.resting = False
+        for cell in game.tilemap.layers['triggers'].collide(new, 'blockers'):
+            blockers = cell['blockers']
+            if 'l' in blockers and last.right <= cell.left < new.right:
+                new.right = cell.left
+            if 'r' in blockers and last.left >= cell.right > new.left:
+                new.left = cell.right
+            if 't' in blockers and last.bottom <= cell.top < new.bottom:
+                self.resting = True
+                new.bottom = cell.top
+                self.dy = 0
+            if 'b' in blockers and last.top >= cell.bottom > new.top:
+                new.top = cell.bottom
+                self.dy = 0
+
+    def update(self, dt, game):
+        last = self.rect.copy()
+
+        key = pygame.key.get_pressed()
+        self._handle_key(key, dt)
+        new = self.rect
+        self._handle_blockers(last, new, game)
+        # if self.resting and key[pygame.K_SPACE]:
         #    self.dy = -500
         #self.dy = min(400, self.dy + 40)
 
         #self.rect.y += self.dy * dt
 
-        new = self.rect
-        self.resting = False
-        for cell in game.tilemap.layers['triggers'].collide(new, 'blockers'):
-            blockers = cell['blockers']
-            if 'l' in blockers and last.right <= cell.left and new.right > cell.left:
-                new.right = cell.left
-            if 'r' in blockers and last.left >= cell.right and new.left < cell.right:
-                    new.left = cell.right
-            if 't' in blockers and last.bottom <= cell.top and new.bottom > cell.top:
-                    self.resting = True
-                    new.bottom = cell.top
-                    self.dy = 0
-            if 'b' in blockers and last.top >= cell.bottom and new.top < cell.bottom:
-                    new.top = cell.bottom
-                    self.dy = 0
         game.tilemap.set_focus(new.x, new.y)
 
         self.groups()[0].camera_x = self.rect.x - 320
